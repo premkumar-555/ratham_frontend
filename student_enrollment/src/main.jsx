d2542a9b259b4732a5d6871f6a1b0f88cad30e94
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-09-30 13:16:55 +0530

    udpated app screen size to mobile screen size through css

--- student_enrollment/src/main.jsx
+++ student_enrollment/src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from './Store/store.js'
 import App from './App.jsx'
+import '../index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
